27e4c3c950508dd87b95793faba1a7c0c2ebc6c3
Mohammadreza Khoshbin <m.khoshbin@live.com>
2021-02-27 20:36:59 +0330

    bumped version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, The PyAuxetic Team'
 author = 'The PyAuxetic Team'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
